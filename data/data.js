6f1622ce87f714c11d2293c18d23836fcbc7501c
leonardo sandoval aya <leonardoaya1234@gmail.com>
2023-03-22 13:36:50 -0500

    ejercicio de practica

---
+++ data/data.js
@@ -0,0 +1,56 @@
+export const data =[
+{
+    question:"Que es un objeto",
+    choices:[
+        'nada',
+        'excel',
+        'todorico',
+        'todo lo que tiene propiededades'
+    ],
+    answer:'sublime'
+},
+{
+    question:"cual es un lenguaje de programacion",
+    choices:[
+        'css',
+        'tyjava',
+        'pascal',
+        'sublime'
+    ],
+    answer:'pascal'
+},
+{
+    question:"para que es css3",
+    choices:[
+        'herramienta',
+        'estilos',
+        'etiquetas',
+        'sublime'
+    ],
+    answer:'etiquetas'
+},
+{
+    question:"para que  programar",
+    choices:[
+        'herramienta',
+        'estilos',
+        'etiquetas',
+        'para encontra una respuesta a un problema'
+    ],
+    answer:'para encantra una respuesta a un problema'
+},
+{
+    question:"que es  una biblioteca ",
+    choices:[
+        'herramienta',
+        ' es una colección de patrones para ayudarte en la construcción de una web,',
+        'para nada',
+        ' es una colección de objetos y funciones que sirven para un propósito en particular.'
+
+        
+    ],
+    answer:'es una colección de objetos y funciones que sirven para un propósito en particular'
+},
+
+
+];
